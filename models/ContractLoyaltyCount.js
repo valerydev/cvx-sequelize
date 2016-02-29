d516ff1e713793d9611cb4bfa8672c74119a311b
Carlos Atencio <carlosatencio@hotmail.com>
2016-02-29 16:53:52 -0430

    Añadido modelo ContractLoyaltyCount

---
+++ models/ContractLoyaltyCount.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, Sequelize) {
+  return [{
+    id: {},
+    contractId: {},
+    sequence: {}
+  }, {
+    classMethods: {
+      associate: function () {
+      }
+    }
+  }];
+};
