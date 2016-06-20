f098d3406877555856c595f286f1fc6bfa91e90b
Raul Contreras <hendrul@gmail.com>
2016-06-20 10:26:09 -0430

    Corregido: Debe saltarse la validacion para campos anadidos automaticamente

--- sequelize-plugins/sequelize-global-hooks/beforeValidate.js
+++ sequelize-plugins/sequelize-global-hooks/beforeValidate.js
@@ -1,3 +1,3 @@
-module.exports = ( instance, opts ) => {
-
+module.exports = function( instance, opts ) {
+  opts.skip = ['contractId', 'classifierId1', 'classifierId2', 'classifierId3', 'branchId'];
 };
