51512cdc253f7fe43d82652ed98b872f62ecfcc8
Raul Contreras <hendrul@gmail.com>
2016-07-07 13:27:58 -0430

    Metodo de asociacion "create<Association>" genera una propiedad en la instancia fuente

--- sequelize-plugins/association-fields.js
+++ sequelize-plugins/association-fields.js
@@ -29,6 +29,35 @@ module.exports = function(sequelize){
       });
     return this;
   };
+
+  var injectCreatorWrapper = function($injectCreator, instancePrototype){
+    var association = this;
+    $injectCreator.call(association, instancePrototype);
+    instancePrototype[association.accessors.create] =
+      _.wrap(instancePrototype[association.accessors.create], function($createAssociation, values, options){
+        var instance = this;
+
+        return $createAssociation.call(instance, values, options)
+          .then(function(data){
+            var attribName = association.as;
+            if(!Object.hasOwnProperty(instance, attribName)) {
+              Object.defineProperty(instance, attribName, {
+                configurable: true,
+                get: function () {
+                  return instance.getDataValue(attribName);
+                },
+                set: function (value) {
+                  return instance.setDataValue(attribName, value);
+                }
+              });
+            }
+            instance.setDataValue(attribName, data);
+            return data;
+          });
+      });
+    return this;
+  };
+
   sequelize.Association.BelongsTo.prototype.injectGetter =
     _.wrap(sequelize.Association.BelongsTo.prototype.injectGetter, injectGetterWrapper);
   sequelize.Association.HasOne.prototype.injectGetter =
@@ -37,4 +66,13 @@ module.exports = function(sequelize){
     _.wrap(sequelize.Association.HasMany.prototype.injectGetter, injectGetterWrapper);
   sequelize.Association.BelongsToMany.prototype.injectGetter =
     _.wrap(sequelize.Association.BelongsToMany.prototype.injectGetter, injectGetterWrapper);
+
+  sequelize.Association.BelongsTo.prototype.injectCreator =
+    _.wrap(sequelize.Association.BelongsTo.prototype.injectCreator, injectCreatorWrapper);
+  sequelize.Association.HasOne.prototype.injectCreator =
+    _.wrap(sequelize.Association.HasOne.prototype.injectCreator, injectCreatorWrapper);
+  sequelize.Association.HasMany.prototype.injectCreator =
+    _.wrap(sequelize.Association.HasMany.prototype.injectCreator, injectCreatorWrapper);
+  sequelize.Association.BelongsToMany.prototype.injectCreator =
+    _.wrap(sequelize.Association.BelongsToMany.prototype.injectCreator, injectCreatorWrapper);
 };
